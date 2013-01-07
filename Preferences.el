b935408911cae3e4481e22870b16e6e60e2d4ea2
Robert Farr <rfarr@cerner.com>
2013-01-07 15:39:20 -0600

    My Aquamacs preferences

---
+++ Preferences.el
@@ -0,0 +1,49 @@
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messags* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(require 'javadoc-help)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(defvar my-packages '(clojure-mode
+                      nrepl
+                      nrepl-ritz))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+;; (add-hook 'nrepl-interaction-mode-hook 'paredit-mode)
+(add-hook 'nrepl-connected-hook 'paredit-mode) 
+
+(autoload 'paredit-mode "paredit"
+      "Minor mode for pseudo-structurally editing Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+(add-hook 'clojure-mode-hook          (lambda () (paredit-mode +1)))
+
+;; Teach compile the syntax of the kibit output
+(require 'compile)
+(add-to-list 'compilation-error-regexp-alist-alist
+         '(kibit "At \\([^:]+\\):\\([[:digit:]]+\\):" 1 2 nil 0))
+(add-to-list 'compilation-error-regexp-alist 'kibit)
+
+;; A convenient command to run "lein kibit" in the project to which
+;; the current emacs buffer belongs to.
+(defun kibit ()
+  "Run kibit on the current project.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile "lein kibit"))
---
+++ customizations.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (modeline . t) (border-color . "#282a2e") (fringe) (mouse-color . "black") (background-mode . dark) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 12) (left-fringe . 4) (cursor-color . "#cc6666") (background-color . "#1d1f21") (foreground-color . "#c5c8c6") (font . "-apple-Lucida_Grande-medium-normal-normal-*-13-*-*-*-p-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(ecb-options-version "2.40")
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(autoface-default ((t (:inherit default))))
+ '(clojure-mode-default ((t (:inherit autoface-default :height 120 :family "Menlo"))) t)
+ '(nrepl-mode-default ((t (:inherit autoface-default :height 120 :family "Menlo"))) t))
